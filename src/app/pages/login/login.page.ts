68b1a4498a5fd517fc32f575d2b12cc1ffd3b0c1
unknown <sidneyonahon.professional@gmail.com>
2019-03-18 09:01:14 +0800

    updates

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -7,9 +7,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+
+  error_messages = {
+    'email': [
+      { type: 'required', message: 'E-mail is required.'},
+      { type: 'minlength', message: 'E-mail length must be longer or equal to 6 characters.'},
+      { type: 'maxlength', message: 'E-mail length must be lower or equal to 50 characters.'},
+      { type: 'pattern', message: 'Please enter a valid e-mail address.'},
+    ],
+    'password': [
+      { type: 'required', message: 'Password is required.'},
+      { type: 'minlength', message: 'Password length must be longer or equal to 6 characters.'},
+      { type: 'maxlength', message: 'Password length must be lower or equal to 30 characters.'},
+      { type: 'pattern', message: 'Password must contain numbers, uppercase and lowercase characters.'},
+    ]
+  }
+
+  constructor(
+    public formBuilder: FormBuilder
+  ) {
+      this.loginForm = this.formBuilder.group({
+        password: new FormControl('', Validators.compose([
+          Validators.required,
+          Validators.minLength(6),
+          Validators.maxLength(30),
+          Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$')
+        ])),
+        email: new FormControl('', Validators.compose([
+          Validators.required,
+          Validators.minLength(6),
+          Validators.maxLength(50),
+          Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9]+.[a-zA-Z0-9-.]+$')
+        ]))
+      });
+    }
 
   ngOnInit() {
   }
 
+  login() {
+    console.log(this.loginForm.value)
+  }
 }
--- src/app/pages/registration/registration.page.ts
+++ src/app/pages/registration/registration.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -7,7 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationPage implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
+
+  constructor(
+    public formBuilder: FormBuilder
+  ) { 
+    this.registerForm = this.formBuilder.group({
+      firstname: new FormControl('', Validators.compose([
+        Validators.required,
+      ])),
+      middlename: new FormControl(''),
+      last: new FormControl(''),
+      lastname: new FormControl('', Validators.compose([
+        Validators.required,
+      ])),
+      email: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(50),
+        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9]+.[a-zA-Z0-9-.]+$')
+      ])),
+      password: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(30),
+        Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$')
+      ]))
+    });    
+  }
 
   ngOnInit() {
   }
